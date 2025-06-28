fbf1db514bf43ee17b7d361d6464fd061ebb9b30
thanavreddy <thanavreddy196@gmail.com>
2025-06-28 23:49:22 +0530

    backend changes

--- backend/app.py
+++ backend/app.py
@@ -79,6 +79,5 @@ def get_entries():
     return jsonify(result)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
